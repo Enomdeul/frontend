31adb9eb8f258dee41b9567e7db790a322cd0b80
seseoju <preengus@gmail.com>
2025-11-20 13:55:43 +0900

    feat: 웹뷰 기반 하이브리드 앱 세팅

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,24 +1,35 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {DarkTheme, DefaultTheme, ThemeProvider} from "@react-navigation/native";
+import {StatusBar} from "expo-status-bar";
+import "react-native-reanimated";
+import {StyleSheet, View} from "react-native";
+import {WebView} from "react-native-webview";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import {useColorScheme} from "@/hooks/use-color-scheme";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
+  const uri = process.env.EXPO_PUBLIC_WEB_URL || "http://localhost:5173";
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <View style={styles.container}>
+        <WebView source={{uri}} style={styles.webview} />
+      </View>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
